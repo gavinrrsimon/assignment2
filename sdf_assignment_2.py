1e0dcf3c26da6f9a75355c3c7c45964de03ad96a
gavinrrsimon <gsimon@academic.rrc.ca>
2024-09-22 07:45:17 -0500

    Created 3 variables with different  data types: string, bool, integer. Displayed the value and data type of each declared variable.

--- sdf_assignment_2.py
+++ sdf_assignment_2.py
@@ -6,6 +6,19 @@ Date: 09/22/2024
 
 #SIMPLE DATA TYPES
 
+name = "gavin"
+print(f"name:", name, "type:",type(name))
+
+driver_license = True
+print(f"value:", driver_license, "type:", type(driver_license))
+
+current_year = 2024
+print(f"this year:", current_year, "type:", type(current_year))
+
+current_year += 1
+print(f"next year:", current_year, "type:", type(current_year))
+
+
 #CALCULATIONS
 
 #LISTS
